9eb46417d2d9ed19bf1aea7ecfe5cefdefdc2fba
Anitha Akkimi <anitha@Anithas-Air.Dlink>
2023-05-17 19:12:11 +0530

    Third Version

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{ width: 500 }}>
+    <Box sx={{ width: 500, position: "sticky", bottom:0 }}>
       <BottomNavigation
         showLabels
         value={value}
